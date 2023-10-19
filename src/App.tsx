c1288bdc7cfed6ea1a2865a1a88f736bfea06bde
jcalhinho <lamaprod93@gmail.com>
2023-10-20 01:49:54 +0200

    map try

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+class BombermanMap extends Phaser.Scene {
+  constructor() {
+    super({ key: 'BombermanMap' });
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  preload() {
+    this.load.image('tile', 'tile.png'); // Assurez-vous de fournir une image appropriée pour les tuiles.
+  }
+
+  create() {
+    const tileWidth = 32; // Largeur d'une tuile (en pixels)
+    const numRows = 16; // Nombre de lignes
+    const numCols = 16; // Nombre de colonnes
+
+    for (let row = 0; row < numRows; row++) {
+      for (let col = 0; col < numCols; col++) {
+        const x = col * tileWidth;
+        const y = row * tileWidth;
+        this.add.image(x, y, 'tile');
+      }
+    }
+  }
 }
 
-export default App
+const BombermanGame = () => {
+  useEffect(() => {
+    const config = {
+      type: Phaser.AUTO,
+      width: 512,
+      height: 512,
+      scene: BombermanMap
+    };
+
+    const game = new Phaser.Game(config);
+
+    return () => {
+      game.destroy(true);
+    }
+  }, []);
+
+  return (
+    <div id="phaser-game" />
+  );
+}
+
+export default BombermanGame;
--- src/main.tsx
+++ src/main.tsx
@@ -1,39 +1,67 @@
 import * as Phaser from 'phaser';
-import logoImg from './assets/jc.png';
-import  "./index.css";
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
+import mur from './assets/mur.png';
+import sol from './assets/sol.png';
+import "./index.css";
+
+
+
+function creerCarte(largeur: number, longueur: number): number[][] {
+    const carte: number[][] = [];
+
+    for (let i = 0; i < longueur; i++) {
+        const ligne: number[] = [];
+        for (let j = 0; j < largeur; j++) {
+            if (i === 0 || i === longueur - 1 || j === 0 || j === largeur - 1) {
+                ligne.push(1); // Ajoute un mur aux bords de la carte
+            } else {
+                ligne.push(0); // Ajoute un espace vide à l'intérieur
+            }
+        }
+        carte.push(ligne);
+    }
+
+    return carte;
+}
+class BombermanGame extends Phaser.Scene {
+    constructor() {
         super();
     }
 
-    preload ()
-    {
-        this.load.image('logo', logoImg);
+    preload() {
+        this.load.image('mur', mur);
+        this.load.image('espaceVide', sol);
+        this.load.image('joueur', 'chemin/vers/image/joueur.png');
+        // Ajoutez d'autres images si nécessaire pour les autres éléments du jeu
     }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
+
+    create() {
+        const tailleTuile = 32; // Taille d'une tuile en pixels
+        const carte = creerCarte(24, 24)
+
+
+        for (let i = 0; i < 24; i++) {
+            for (let j = 0; j < 24; j++) {
+                const tileType = carte[i][j];
+                const x = j * tailleTuile;
+                const y = i * tailleTuile;
+                if (tileType === 1) {
+                    this.add.image(x, y, 'mur').setOrigin(0, 0);
+                } else if (tileType === 2) {
+                    this.add.image(x, y, 'joueur').setOrigin(0, 0);
+                } else {
+                    this.add.image(x, y, 'espaceVide').setOrigin(0, 0);
+                }
+            }
+        }
     }
 }
 
 const config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
+    width: 768, // 24 tuiles x 32 pixels
+    height: 768, // 24 tuiles x 32 pixels
+    scene: BombermanGame
 };
 
 const game = new Phaser.Game(config);
